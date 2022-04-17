97e6a572daad6403c70ca4f434bb5fdaf182b2df
Noah Grocholl <noahgrocholl@gmail.com>
2022-04-17 17:47:29 +0200

    carousel und weiter verbesserungen -> responsive

--- content.php
+++ content.php
@@ -1,10 +1,12 @@
-<div>
+<div class="mb-5">
     <div class="row my-2 d-flex justify-content-center">
-        <img src="images/pizza.jpeg" alt="" style="width: 90%; height: 30rem">
+        <img class="d-md-none" src="images/pizza.jpeg" alt="" style="width: 90%;">
+        <img class="d-none d-md-block d-lg-none" src="images/pizza_md.jpeg" alt="" style="width: 90%;">
+        <img class="d-none d-lg-block" src="images/pizza_lg.jpeg" alt="" style="width: 90%;">
     </div>
 
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6 col-md-6">
             <div class="d-flex justify-content-center">
                 <h3>Wir bieten:</h3>
             </div>
@@ -17,7 +19,7 @@
             </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-lg-6 col-md-6">
             <div class="d-flex justify-content-center">
                 <h3>Haben aber dennoch:</h3>
             </div>
@@ -29,7 +31,80 @@
                 </ul>
             </div>
         </div>
-        
+
     </div>
 </div>
+
+<div class="mb-3 mx-auto" style="width: 90%;">
+
+    <h2 style="font-family: Beau Rivage;">Unsere Spezialitäten</h3>
+
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="d-md-block d-lg-none">
+                        <img src="images/erdbeer.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Erdbeeren mit Scheiße</h5>
+                            <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
+                        </div>
+                    </div>
+                    <div class="d-none d-lg-block" style="width: 64rem; height: 36rem;">
+                        <img src="images/erdbeer.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Erdbeeren mit Scheiße</h5>
+                            <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="d-md-block d-lg-none">
+                        <img src="images/bohnen.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Blaue Bohnen</h5>
+                            <p>Fragen sie nicht warum die blau sind.</p>
+                        </div>
+                    </div>
+                    <div class="d-none d-lg-block" style="width: 64rem; height: 36rem;">
+                        <img src="images/bohnen.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Blaue Bohnen</h5>
+                            <p>Fragen sie nicht warum die blau sind.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="d-md-block d-lg-none">
+                        <img src="images/fisch.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Blobfisch</h5>
+                            <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
+                        </div>
+                    </div>
+                    <div class="d-none d-lg-block" style="width: 64rem; height: 36rem;">
+                        <img src="images/fisch.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption">
+                            <h5>Blobfisch</h5>
+                            <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 </div>
