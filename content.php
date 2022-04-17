59fc9cd13ebbcc3bcd0157440bdacd5170ba4124
Noah Grocholl <noahgrocholl@gmail.com>
2022-04-17 18:10:45 +0200

    formatierung

--- content.php
+++ content.php
@@ -35,90 +35,92 @@
     </div>
 </div>
 
+<!-- Bootstrap Caorusel nicht large -->
 <div class="d-md-block d-lg-block d-xl-none mb-3 mx-auto" style="width: 90%;">
 
     <h2 style="font-family: Beau Rivage;">Unsere Spezialitäten</h3>
 
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/erdbeer.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Erdbeeren mit Scheiße</h5>
+                        <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/bohnen.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Blaue Bohnen</h5>
+                        <p>Fragen sie nicht warum die blau sind.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/fisch.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Blobfisch</h5>
+                        <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="images/erdbeer.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Erdbeeren mit Scheiße</h5>
-                    <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/bohnen.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Blaue Bohnen</h5>
-                    <p>Fragen sie nicht warum die blau sind.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/fisch.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Blobfisch</h5>
-                    <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
 </div>
 
+<!-- Bootstrap Caorusel large -->
 <div class="d-xl-block d-lg-none d-md-none d-none mb-3 mx-auto" style="width: 64rem; height: 36rem">
 
     <h2 style="font-family: Beau Rivage;">Unsere Spezialitäten</h3>
 
-    <div id="carouselExampleCaptions_lg" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div id="carouselExampleCaptions_lg" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions_lg" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/erdbeer.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Erdbeeren mit Scheiße</h5>
+                        <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/bohnen.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Blaue Bohnen</h5>
+                        <p>Fragen sie nicht warum die blau sind.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/fisch.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Blobfisch</h5>
+                        <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="images/erdbeer.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Erdbeeren mit Scheiße</h5>
-                    <p>Sie werden selbst gepflückt und von mir verfeinert.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/bohnen.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Blaue Bohnen</h5>
-                    <p>Fragen sie nicht warum die blau sind.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/fisch.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Blobfisch</h5>
-                    <p>blobfisch blobfisch blobfisch blobfisch blobfisch blobfisch </p>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
 </div>
