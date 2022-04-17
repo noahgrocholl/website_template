2ffa0f4adf07f43f3a70e7c69022072b20b79e3b
Noah Grocholl <noahgrocholl@gmail.com>
2022-04-17 15:56:18 +0200

    funktionierende Tabelle Erstes Bild

--- content.php
+++ content.php
@@ -1,24 +1,35 @@
 <div>
     <div class="row my-2 d-flex justify-content-center">
-        <img src="images/pizza.jpeg" alt="" style="width: 90%; height: 20rem">
+        <img src="images/pizza.jpeg" alt="" style="width: 90%; height: 30rem">
     </div>
 
     <div class="row">
-        <div class="col-6 d-flex justify-content-center">
-            <h3 class="text-left">Wir bieten:</h3>
-            <ul class="text-left">
-                <li>Sehr hochwertige moderne Küche</li>
-                <li>nur die besten Zutaten</li>
-                <li>schnell zubereitete Mahlzeiten</li>
-            </ul>
-</div>
-        <div class="col-6 d-flex justify-content-center">
-            <h3 class="text-left">Haben aber dennoch:</h3>
-            <ul class="text-left">
-                <li>ein freundliches Personal</li>
-                <li>frische Zuaten aus der Region</li>
-                <li>die Einhaltung aller Hygenestandarts</li>
-            </ul>
+        <div class="col-6">
+            <div class="d-flex justify-content-center">
+                <h3>Wir bieten:</h3>
+            </div>
+            <div class="d-flex justify-content-center">
+                <ul>
+                    <li>Sehr hochwertige moderne Küche</li>
+                    <li>nur die besten Zutaten</li>
+                    <li>schnell zubereitete Mahlzeiten</li>
+                </ul>
+            </div>
         </div>
+
+        <div class="col-6">
+            <div class="d-flex justify-content-center">
+                <h3>Haben aber dennoch:</h3>
+            </div>
+            <div class="d-flex justify-content-center">
+                <ul>
+                    <li>ein freundliches Personal</li>
+                    <li>frische Zuaten aus der Region</li>
+                    <li>die Einhaltung aller Hygenestandarts</li>
+                </ul>
+            </div>
+        </div>
+        
     </div>
+</div>
 </div>
