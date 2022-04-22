30d082c32e0e897dd20a038217cc62aed5862f6f
Noah Grocholl <noahgrocholl@gmail.com>
2022-04-22 21:57:35 +0200

    Restaurant nur Salat ABER FUNKTIONIERT

--- restaurant_menu.php
+++ restaurant_menu.php
@@ -15,11 +15,12 @@ function create_cards($width, $height, $category)
                 $items_cards,
                 array(
                     '<div class="card" style="width: ' . $width . 'rem; height: ' . $height . 'rem">
-                    <div class="card-body">
-                        <h5 class="card-title">' . $item[2] . '</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">' . $item[3] . '</h6>
-                    </div>
-            </div>', $category
+                        <div class="card-body">
+                            <h5 class="card-title">' . $item[2] . '</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">' . $item[3] . '</h6>
+                        </div>
+                    </div>',
+                    $category, $item[1]
                 )
             );
         }
@@ -29,42 +30,56 @@ function create_cards($width, $height, $category)
 
 function show_items($number, $divider, $width, $height)
 {
-    $db = new Db;
-    $menu_items = $db->query("SELECT * from menu_items");
-
     $items_salad = create_cards($width, $height, 1);
     $items_pizza = create_cards($width, $height, 2);
     $items_pasta = create_cards($width, $height, 3);
     $items_grill = create_cards($width, $height, 4);
     $items_cards = array($items_salad, $items_pizza, $items_pasta, $items_grill);
 
-    $items_count = array();
-    foreach ($items_cards as $cards) {
-        foreach ($cards as $card) {
-            if (count($items_count) == $number) { ?>
+    $count_items = [];
+
+    foreach ($items_salad as $key => $card) {
+
+        if ($number == 1) { ?>
+            <div class="d-flex justify-content-center">
+                <div class="row mb-2">
+                    <div class="col-<?= $divider ?>">
+
+                        <?php
+                        echo $card[0];
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php
+        }
+
+        if ($number == 3) {
+            array_push($count_items, $card);
+            if (($key + 1) % 3 == 0 || ($key + 1) == 16) { ?>
                 <div class="d-flex justify-content-center">
                     <div class="row mb-2">
-                        <?php
-                        foreach ($items_count as $item) { ?>
+                        <?php foreach ($count_items as $item) { ?>
                             <div class="col-<?= $divider ?>">
 
                                 <?php
-                                echo $item[1];
+                                echo $item[0];
                                 ?>
 
                             </div>
-
                         <?php } ?>
                     </div>
                 </div>
 <?php
-                $items_count = array();
-            } else {
-                array_push($items_count, $card);
+                $count_items = [];
             }
         }
     }
 }
+
+
 ?>
 
 <div class="d-md-none">
